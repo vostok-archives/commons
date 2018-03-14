6acbc881cb99f1633a0bf367f01c3ba997eac52d
razboynikov <razboynikov@skbkontur.ru>
2018-03-14 16:20:05 +0500

    Formatted tab -> spaces.

--- Vostok.Commons/ThreadManagment/ThreadRunner.cs
+++ Vostok.Commons/ThreadManagment/ThreadRunner.cs
@@ -3,28 +3,28 @@ using System.Threading;
 
 namespace Vostok.Commons.ThreadManagment
 {
-	public static class ThreadRunner
-	{
-		public static Thread Run(Action threadRoutine, Action<Thread> tuneThreadBeforeStart = null)
-		{
-			var t = new Thread(ThreadRoutineWrapper.Wrap(threadRoutine))
-			{
-				IsBackground = true
-			};
+    public static class ThreadRunner
+    {
+        public static Thread Run(Action threadRoutine, Action<Thread> tuneThreadBeforeStart = null)
+        {
+            var t = new Thread(ThreadRoutineWrapper.Wrap(threadRoutine))
+            {
+                IsBackground = true
+            };
             tuneThreadBeforeStart?.Invoke(t);
             t.Start();
-			return t;
-		}
+            return t;
+        }
 
-		public static Thread Run(Action<object> threadRoutine, object threadRoutineParameter, Action<Thread> tuneThreadBeforeStart = null)
-		{
-			var t = new Thread(ThreadRoutineWrapper.Wrap(threadRoutine))
-			{
-				IsBackground = true
-			};
+        public static Thread Run(Action<object> threadRoutine, object threadRoutineParameter, Action<Thread> tuneThreadBeforeStart = null)
+        {
+            var t = new Thread(ThreadRoutineWrapper.Wrap(threadRoutine))
+            {
+                IsBackground = true
+            };
             tuneThreadBeforeStart?.Invoke(t);
             t.Start(threadRoutineParameter);
-			return t;
-		}
-	}
+            return t;
+        }
+    }
 }
