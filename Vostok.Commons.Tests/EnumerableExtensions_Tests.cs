f892dc7364f85d23b2bb271e59a2c56cf2df84c2
razboynikov <razboynikov@skbkontur.ru>
2018-03-21 20:43:10 +0500

    Language version fixes. Test fixes. EnumberableExtensions added.

---
+++ Vostok.Commons.Tests/EnumerableExtensions_Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Vostok.Commons.Tests
+{
+    [TestFixture]
+    public class EnumerableExtensions_Tests
+    {
+        [Test]
+        public void Should_extend_ToEnumerable()
+        {
+            var val = 1;
+            val.ToEnumerable().ToArray().Should().BeEquivalentTo(new[] {1});
+        }
+    }
+}
--- Vostok.Commons.Tests/ThreadManagment/ThreadRunner_Tests.cs
+++ Vostok.Commons.Tests/ThreadManagment/ThreadRunner_Tests.cs
@@ -17,26 +17,34 @@ namespace Vostok.Commons.Tests.ThreadManagment
         [Test]
         public void Run_should_run_thread()
         {
-            new Action(Run_should_run_thread_test).ShouldPassIn(300.Milliseconds());
+            new Action(Run_should_run_thread_test).ShouldPassIn(400.Milliseconds());
         }
         public void Run_should_run_thread_test()
         {
-            var thread = ThreadRunner.Run(() => SomeValue = 123);
+            var thread = ThreadRunner.Run(() =>
+            {
+                SomeValue = 123;
+                Thread.Sleep(200);
+            });
             thread.IsBackground.Should().BeTrue();
-            Thread.Sleep(200);
+            Thread.Sleep(300);
             SomeValue.Should().Be(123);
         }
 
         [Test]
         public void Run_should_run_thread_with_parameters()
         {
-            new Action(Run_should_run_thread_with_parameters_test).ShouldPassIn(300.Milliseconds());
+            new Action(Run_should_run_thread_with_parameters_test).ShouldPassIn(400.Milliseconds());
         }
         public void Run_should_run_thread_with_parameters_test()
         {
-            var thread = ThreadRunner.Run(obj => SomeValueParam = (int) obj, 321);
+            var thread = ThreadRunner.Run(obj =>
+            {
+                SomeValueParam = (int)obj;
+                Thread.Sleep(200);
+            }, 321);
             thread.IsBackground.Should().BeTrue();
-            Thread.Sleep(200);
+            Thread.Sleep(300);
             SomeValueParam.Should().Be(321);
         }
     }
