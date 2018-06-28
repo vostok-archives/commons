465bc870dfe3fb0b5db8167dd997b7b7ee0cc61d
razboynikov <razboynikov@skbkontur.ru>
2018-06-28 18:34:27 +0500

    Added TaskExtensions.

---
+++ Vostok.Commons/Extensions/TaskExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Vostok.Commons.Extensions
+{
+    public static class TaskExtensions
+    {
+        public static Task SilentlyContinue(this Task source) => source.ContinueWith(_ => { });
+    }
+}
